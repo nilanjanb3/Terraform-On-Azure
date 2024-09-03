63e7e33c48be026c78a463aa06edf17add6d28b7
Nilanjan Bhattacharjee <nilanjan.bhattacharjee77@gmail.com>
2024-09-03 18:47:37 +0530

    added modularity

--- manifests/04-azure-linux-vm/app-scripts/ssh-keygen.sh
+++ manifests/04-azure-linux-vm/app-scripts/ssh-keygen.sh
@@ -6,4 +6,6 @@ ssh-keygen \
  -t rsa \
  -b 4069 \
  -C "azureuser@myserver" \
- -f $(pwd)/ssh-keys/terraform-azure.pub
+ -f $(pwd)/ssh-keys/terraform-azure.pem
+
+mv ./ssh-keys/terraform-azure.pem.pub ./ssh-keys/terraform-azure.pub
